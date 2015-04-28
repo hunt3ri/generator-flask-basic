32c51e99e6d6a8d144443d38371f0f41db5326b7
Iain Hunter <iain.hunter@outlook.com>
2015-04-28 22:09:38 +0100

    working basic gen

--- app/index.js
+++ app/index.js
@@ -15,20 +15,44 @@ module.exports = yeoman.generators.Base.extend({
       this.log(yosay(
           'Welcome to the ' + chalk.red('Python Flask Basics') + ' generator!'
       ));
-      console.log('test it1');
-      done();
-  },
 
+      var prompts = [{
+      type: 'input',
+      name: 'appName',
+      message: "What's the name of your app: ",
+      default: function() {return 'Flask-Basic'}
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.appName = props.appName;
+
+      done();
+    }.bind(this));
+
+  },
 
     writing: {
         app: function () {
-            console.log('hello');
-            this.mkdir('app');
-            this.copy('_static', 'app/static');
+            console.log('Scaffolding Flask Basic app in directory : ' + this.appName)
+            this.mkdir(this.appName);
+            var appDir = this.appName + "/app";
+            this.mkdir(appDir);
+            this.bulkDirectory('_static', appDir + '/static', null);
+            this.bulkDirectory('_templates', appDir + '/templates', null);
+            this.bulkDirectory('_web', appDir + '/web', null)
+            this.copy('__init__.py', appDir + '/__init__.py', null);
+            this.copy('_root/manage.py', this.appName + '/manage.py', null);
+            this.copy('_root/.gitignore', this.appName + '/.gitignore', null);
+            this.copy('_root/requirements.txt', this.appName + '/requirements.txt', null);
+
         }
-    }
+    },
 
-
+  install: function () {
+      console.log("Hello");
+      this.spawnCommand('pyvenv', [this.appName + '/venv']);
+      /*this.spawnCommand('source', ['./' + this.appName + '/venv/bin/activate']);*/
+  }
 
 
 });
