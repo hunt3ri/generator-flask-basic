cc4c63b93c56ca3a7670391dfcdacc2bb8df261a
Iain Hunter <iain.hunter@skyscanner.net>
2015-05-23 08:08:28 +0100

    Add directive

---
+++ app/templates/_static/scripts/directives.js
@@ -0,0 +1,10 @@
+'use strict';
+angular.module('myApp.directives', [])
+.directive('helloWorld', function () {
+    return {
+        restrict: 'AE',
+        scope: { greeting: "=name" },
+        template: "<h1>Hello {[ greeting.name ]}</h1>" +
+                  "<input data-ng-model='greeting.name'></input>"
+        }
+    });
