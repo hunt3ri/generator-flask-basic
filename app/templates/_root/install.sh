c1ecae7dc30d0580f467e02098687b56a76d9b9c
Iain Hunter <iain.hunter@skyscanner.net>
2015-05-23 09:29:37 +0100

    Add install script

---
+++ app/templates/_root/install.sh
@@ -0,0 +1,28 @@
+echo "Creating Virtual Env"
+echo "===================="
+echo ""
+
+deactivate  &>/dev/null
+pyvenv venv
+source ./venv/bin/activate
+
+echo ""
+echo "Installing Python Dependencies"
+echo "=============================="
+echo ""
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo ""
+echo "Installing Javascript Dependencies"
+echo "==============================="
+echo ""
+
+npm install
+
+echo ""
+echo "Congrats all dependencies installed"
+echo "Grunt takes over from here, type - grunt build - to get started"
+echo "You can type - python manage.py runserver -d - to start up the app"
+echo ""
